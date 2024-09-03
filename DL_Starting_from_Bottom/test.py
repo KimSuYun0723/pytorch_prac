1d5d7b0638c465be9107e2482885d48835fe74e0
suyun kim <202200737@hufs.ac.kr>
2024-09-03 20:30:11 +0900

    Update: basic pytorch

---
+++ DL_Starting_from_Bottom/test.py
@@ -0,0 +1,38 @@
+data = """A woman has been left isolated and unable to leave her house after the only Covid vaccine she could safely receive was withdrawn from the NHS booster programme.
+Diana Tasker, 78, was allergic to both the Pfizer and Moderna vaccines, so had previously been given an alternative booster jab.
+Ms Tasker, from Scarborough, said she had no choice but to go into "indefinite lockdown" after the Novavax booster became unavailable.
+The UK Health Security Agency (UKHSA) confirmed that Novavax had not been offered in the spring top-up programme and had been replaced by more "cost-effective" vaccines.
+
+Ms Tasker said: "I should have had a vaccine this spring, but when I rang Castle Health Centre to book it, they told me the government had stopped Novavax and they were only making Pfizer and Moderna available.
+"I had no problems with this alternative vaccine at all."
+Dr Mary Ramsay, head of public health programmes at UKHSA, said new guidance had been issued for mRNA allergy patients.
+They were now to be "seen by an expert allergist and should then be vaccinated in hospital under clinical supervision".
+For Ms Tasker, this would mean having a Pfizer or Moderna booster in hospital, where she would be monitored if she went into anaphylactic shock.
+However, she is also allergic to adrenaline, which could be used to treat her if she suffered an adverse reaction.
+"I would never go against my doctor’s advice," Ms Tasker said.
+Diana Tasker going through print outs of letters she has written
+Ms Tasker has written to about 30 people and organisations for answers
+
+'Prison sentence'
+Ms Tasker has sent letters to numerous organisations to try and find answers to her predicament.
+"I have been trying since April to find out where I could get a vaccine and why it has been denied to me," Ms Tasker told the BBC.
+"I’ve now got a folder full of letters to all the different departments, very few of which replied, and nobody has given me an answer as to why they have stopped an excellent system."
+She has now opted to self-isolate rather than risk leaving home without a booster vaccine.
+"It’s worse than a prison sentence because with a prison sentence, you know how long you’ve got in captivity," she added.
+"We don’t know how long it will be before we can get this put right."
+Diana Tasker looking out the living room window of her Scarborough home
+Ms Tasker hoped to spend the summer swimming and going out with friends
+
+'Not giving up'
+Ms Tasker explained that she had missed spending time outdoors during the previous four summers due to having cancer and problems with her heart.
+She hoped this year would be a "sweet reward" after some difficult years.
+"I love to go swimming and hear our Scarborough Spa Orchestra," she said.
+"Just the sheer freedom of going and meeting friends and socialising, I don’t know when or if I’ll be able to do it again."
+She called for "immediate action" to reinstate the previous system.
+The retired teacher has previously campaigned to protect buildings including the Scarborough Spa and the Futurist Theatre.
+"I've worked on other campaigns throughout my life and I thought I'd retired from them," Ms Tasker said.
+"But this campaign is the most important of the lot because this one is dealing with people's lives.
+“I’m not going to give up and I’m not going to go away.”
+Follow BBC Yorkshire on Facebook, X (formerly Twitter) and Instagram. Send your story ideas to yorkslincs.news@bbc.co.uk"""
+
+print(data)
